928ae2730007b829785bf7a37981b51bd9909ff1
Stewart MacKenzie-Leigh <stewart@stugo.co.uk>
2016-04-14 14:47:41 +0100

    Add convenience exports

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-var Schema = require('./dist/Schema').default;
-var Validator = require('./dist/Validator').default;
-
-module.exports = Schema;
-module.exports.Validator = Validator;
+module.exports = require('./dist/Schema').default;
+module.exports.Validator = require('./dist/Validator').default;
+module.exports.AnyType = require('./dist/types/AnyType').default;
+module.exports.error = require('./dist/util').error;
+module.exports.isEmpty = require('./dist/util').isEmpty;
