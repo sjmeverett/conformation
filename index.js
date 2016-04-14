bcc7393260b929baada2e5ffd4b4b3238fbb90a2
Stewart MacKenzie-Leigh <stewart@stugo.co.uk>
2016-04-14 16:04:58 +0100

    Fix packaging

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 module.exports = require('./dist/Schema').default;
-module.exports.Validator = require('./dist/Validator').default;
 module.exports.AnyType = require('./dist/types/AnyType').default;
 module.exports.error = require('./dist/util').error;
 module.exports.isEmpty = require('./dist/util').isEmpty;
